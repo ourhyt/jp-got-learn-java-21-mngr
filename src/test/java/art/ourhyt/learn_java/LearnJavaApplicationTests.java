67e5f319d375044f149f88f7f3c056574b10cbc8
Juan Pablo Florez <jflor17@bancodebogota.com.co>
2025-08-01 13:36:31 -0500

    add first project java

---
+++ src/test/java/art/ourhyt/learn_java/LearnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package art.ourhyt.learn_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
